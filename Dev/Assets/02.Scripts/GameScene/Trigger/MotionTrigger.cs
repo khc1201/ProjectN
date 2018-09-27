797cf450aa1ef11bf7e446cf8864b8f175cf4d5f
khc1201 <41064972+khc1201@users.noreply.github.com>
2018-09-27 23:37:59 +0900

    trigger 제작 중
    DO #3

---
+++ Dev/Assets/02.Scripts/GameScene/Trigger/MotionTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionTrigger : MonoBehaviour
+{
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
+    //target 의 회전, fadeout, fadein 등의 활동을 함
+
+}
---
+++ Dev/Assets/02.Scripts/GameScene/Trigger/TriggerUnit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TriggerUnit : MonoBehaviour
+{
+    #region 기본 영역
+    public string index;
+    public string desc;
+    public bool IsValid = false; // 로딩 영역에서 다루는 인수
+    public bool IsTriggerd = false; // 로딩 영역에서 다루는 인수
+    #endregion
+
+    public bool ShowButton;
+    public GameObject[] Show_Button;
+    public bool HideButton;
+    public GameObject[] Hide_Button;
+    public bool ShowTrigger;
+    public string[] Show_TriggerName;
+    public bool HideTrigger;
+    public string[] Hide_TriggerName;
+    public bool PlayMotion;
+    public MotionTrigger Play_Motion;
+    public bool PlaySound;
+    public string Play_Sound;
+    public bool PlayEffect;
+    public string Play_Effect;
+    public bool SendTrigger;
+    public string Send_TriggerName;
+
+    public void Start()
+    {
+        if (IsTriggerd)
+        {
+            // do Trigger;
+        }
+    }
+
+
+}
