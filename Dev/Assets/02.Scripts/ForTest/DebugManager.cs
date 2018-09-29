a228fa4c0add7c253f9c56f63c5c55cdaf221617
khc1201 <41064972+khc1201@users.noreply.github.com>
2018-09-29 18:10:42 +0900

    Debug Manager 추가
    1. 테스트 환경을 위한 Debug Manager 추가
      - 현재 추가 된 기능 : 현재의 View 의 인덱스와 이름을 보여줌

---
+++ Dev/Assets/02.Scripts/ForTest/DebugManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugManager : MonoBehaviour,IListener {
+    public void Start()
+    {
+        ViewDebug.SetActive(IsViewDebug);
+        csEventManager.Instance.AddListener(EVENT_TYPE.SHOW_HUD, this);
+    }
+
+    public bool IsViewDebug = true;
+    public GameObject ViewDebug;
+    public Text ViewIndex;
+    public Text ViewDesc;
+
+    public void RefreshDebug(string viewIndex, string viewDesc)
+    {
+        if (!IsViewDebug) return;
+
+        ViewIndex.text = viewIndex;
+        ViewDesc.text = viewDesc;
+    }
+
+    public void OnEvent(EVENT_TYPE et, Component sender, object param = null)
+    {
+        if(et == EVENT_TYPE.SHOW_HUD)
+        {
+            ViewObj target = param as ViewObj;
+            RefreshDebug(target._index.ToString(), target._name);
+        }
+    }
+}
