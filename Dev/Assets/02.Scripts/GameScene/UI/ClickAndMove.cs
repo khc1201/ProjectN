5e44a0ae0896aa97190ab97ef8b42cf9f724ce0e
khc1201 <41064972+khc1201@users.noreply.github.com>
2018-10-06 22:20:57 +0900

    요청사항 적용 중
    View 추가
    서랍 여닫이 기능 추가
    쿠션 올렸다 내리기 기능 추가

---
+++ Dev/Assets/02.Scripts/GameScene/UI/ClickAndMove.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ClickAndMove : MonoBehaviour {
+    public GameObject targetObj;
+    private Button buttonUI;
+    public List<Button> otherButtonUI;
+    private Vector3 buttonInitPos;
+    public float moveSpeed;
+    public Vector3 initPos;
+    public Vector3 movePos;
+    public float moveY;
+    public bool IsUsed = false;
+    private Tween tween;
+    public void OnEnable()
+    {
+        IsUsed = false;
+        initPos = targetObj.transform.localPosition;
+        buttonUI = this.gameObject.GetComponent<Button>();
+        buttonInitPos = buttonUI.transform.localPosition;
+        SetButton();
+    }
+    public void OnDisable()
+    {
+        buttonUI.transform.localPosition = buttonInitPos;
+        targetObj.transform.localPosition = initPos;
+        IsUsed = false;
+        foreach (var e in otherButtonUI)
+        {
+            e.enabled = true;
+        }
+    }
+    private void SetButton()
+    {
+        buttonUI.onClick.RemoveAllListeners();
+        buttonUI.onClick.AddListener(OnClickMove);
+    }
+    public void OnMove()
+    {
+        buttonUI.enabled = false;
+         
+    }
+    public void EndMove()
+    {
+        buttonUI.enabled = true;
+    }
+    public void OnClickMove()
+    {
+        if (!IsUsed)
+        {
+            foreach(var e in otherButtonUI)
+            {
+                e.enabled = false;
+            }
+            //buttonUI.gameObject.transform.position = Camera.current.WorldToScreenPoint(movePos);
+            tween = targetObj.transform.DOLocalMove(movePos, moveSpeed).OnStart(OnMove).OnComplete(EndMove).Play();
+            buttonUI.transform.localPosition = new Vector2(this.transform.localPosition.x, this.transform.localPosition.y + moveY);
+            IsUsed = true;
+        }
+        else
+        {
+            foreach (var e in otherButtonUI)
+            {
+                e.enabled = true;
+            }
+            //buttonUI.transform.position = Camera.current.WorldToScreenPoint(initPos);
+            buttonUI.transform.localPosition = new Vector2(this.transform.localPosition.x, buttonInitPos.y);
+            tween = targetObj.transform.DOLocalMove(initPos, moveSpeed).OnStart(OnMove).OnComplete(EndMove).Play();
+            IsUsed = false;
+        }
+    }
+
+}
---
+++ Dev/Assets/02.Scripts/GameScene/UI/MoveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public GameObject target;
+    public MOTION_TYPE motionType;
+    #region MOTION_TYPE : ROTATE
+    public float rotateSpeed;
+    public Vector3 rotateValue;
+    #endregion
+
+    #region MOTION_TYPE : FADE
+    public float fadeTime;
+    #endregion
+
+    #region MOTION_TYPE : MOVE
+    public float moveSpeed;
+    public Vector3 movePos;
+    #endregion
+
+    public void OnClickMotion()
+    {
+    }
+}
