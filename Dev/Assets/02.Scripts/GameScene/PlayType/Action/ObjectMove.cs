d6cdefaeed22f8ca48ea3caff8e4371289e01f6b
khc1201 <41064972+khc1201@users.noreply.github.com>
2018-10-29 00:58:29 +0900

    기타 배치
    기타 배치

---
+++ Dev/Assets/02.Scripts/GameScene/PlayType/Action/ObjectMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ObjectMove : MonoBehaviour {
+
+    public GameObject targetObject;
+    //public Vector3 targetInitRot;
+    public Vector3 targetMove;
+    public float moveTime;
+
+    public void Start()
+    {
+        if (targetObject == null)
+        {
+            Debug.LogError("targetObject 가 없습니다. : " + this.gameObject.name);
+            return;
+        }
+        
+        targetObject.transform.DOLocalMove(targetMove, moveTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo).Play();
+    }
+}
---
+++ Dev/Assets/02.Scripts/GameScene/PlayType/Action/ObjectRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ObjectRotate : MonoBehaviour {
+
+    public GameObject targetObject;
+    //public Vector3 targetInitRot;
+    public Vector3 targetRot;
+    public float rotTime;
+
+    public void Start()
+    {
+        if(targetObject == null)
+        {
+            Debug.LogError("targetObject 가 없습니다. : " + this.gameObject.name);
+            return;
+        }
+        
+        targetObject.transform.DOLocalRotate(targetRot, rotTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo).Play();
+    }
+
+    
+}
